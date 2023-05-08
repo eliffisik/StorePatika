a0cc2e0de835fc03e94b27e2ed434e3c77d123de
eliffisik <eliffisik@icloud.com>
2023-05-08 20:01:43 +0300

    filesadded

---
+++ src/App.js
@@ -0,0 +1,4 @@
+import React from "react";
+import {View, StyleSheet, TextInput, FlatList} from 'react-native';
+import product_data from "./product-data.json";
+import PatiCard from "./components/PatiCard/PatiCard";
